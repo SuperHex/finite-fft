16b6b3888c3def03dcb9869310930d1ff8f9805c
Ruihan Wang <wang_ruihan@hotmail.com>
2019-01-30 01:54:21 -0500

    fast ifft

--- app/Main.hs
+++ app/Main.hs
@@ -11,4 +11,7 @@ instance Finite Field 65537
 
 main :: IO ()
 main =
-  print $ fft (Field @Int 5) (coerce @[Int] @[Field Int] [0 .. 65535]) ! 65535
+  let v =
+        ifft (Field @Int 5) . fft (Field @Int 5) $ coerce @[Int] @[Field Int]
+          [0 .. 65535]
+  in  print $ v ! 65535
--- src/FFT.hs
+++ src/FFT.hs
@@ -2,18 +2,18 @@
 {-# LANGUAGE ExplicitForAll #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module FFT where
 
-import Control.Parallel
-import Data.Bits
-import FiniteField
-import GHC.TypeLits
+import           Control.Monad
+import           Control.Monad.ST
+import           Data.Bits
+import           Data.STRef
+import qualified Data.Vector         as V
 import qualified Data.Vector.Mutable as MV
-import qualified Data.Vector as V
-import Control.Monad.ST
-import Control.Monad
-import Data.STRef
+import           FiniteField
+import           GHC.TypeLits
 
 padding :: a -> [a] -> [a]
 padding x xs = if len .&. (len - 1) == 0
@@ -47,8 +47,11 @@ oddEven v = case MV.length v of
       MV.unsafeWrite ev idx =<< MV.unsafeRead v i
       MV.unsafeWrite od idx =<< MV.unsafeRead v (i + 1)
     return (od, ev)
-  -- assume the input vector's length is power of 2
 
+{-# SPECIALIZE
+    fft :: forall n . (Finite Field n)
+        => Field Int -> [Field Int] -> V.Vector (Field Int)
+  #-}
 fft
   :: forall n f a
    . (KnownNat n, Finite f n, Integral a)
@@ -83,33 +86,58 @@ fft root p = runST $ do
         MV.unsafeWrite v (ix + k) (bi .- (m' .* ci))
         modifySTRef' m (.* r)
       return v
-        -- let out = V.zipWith (.+) b
-        --       $ V.zipWith
-        --       (.*)
-        --       c
-        --       [ root .^ fromIntegral n | n <- [0 .. length p `div` 2 - 1] ]
-        --     out' = zipWith (.+) b $ zipWith
-        --       (.*)
-        --       c
-        --       [ root .^ fromIntegral n | n <- [length p `div` 2 .. length p - 1] ]
-        -- b `par` c `pseq` out ++ out'
+
+-- ifft
+--   :: forall n f a
+--    . (KnownNat n, Finite f n, Integral a)
+--   => f a
+--   -> [f a]
+--   -> [f a]
+-- ifft root l =
+--   let len = length l
+--       matrix =
+--         [ [ root .^ (-i * j) | i <- [0 .. len - 1] ] | j <- [0 .. len - 1] ]
+--   in  fmap
+--         ( (.* (inverse . pure . fromIntegral) len)
+--         . foldl1 (.+)
+--         . flip (zipWith (.*)) l
+--         )
+--         matrix
 
 ifft
   :: forall n f a
    . (KnownNat n, Finite f n, Integral a)
   => f a
-  -> [f a]
-  -> [f a]
-ifft root l =
-  let len = length l
-      matrix =
-        [ [ root .^ (-i * j) | i <- [0 .. len - 1] ] | j <- [0 .. len - 1] ]
-  in  fmap
-        ( (.* (inverse . pure . fromIntegral) len)
-        . foldl1 (.+)
-        . flip (zipWith (.*)) l
-        )
-        matrix
+  -> V.Vector (f a)
+  -> V.Vector (f a)
+ifft root v = runST $ do
+  mv <- V.thaw v
+  v' <- worker root mv
+  let len = fromIntegral (V.length v)
+      inv = inverse (pure len)
+  forM_ [0 .. V.length v - 1] $ \ix -> do
+    MV.unsafeModify v' (.* inv) ix
+  V.freeze v'
+ where
+  worker :: f a -> MV.STVector s (f a) -> ST s (MV.STVector s (f a))
+  worker r v = case MV.length v of
+    0 -> return v
+    1 -> return v
+    n -> do
+      (od, ev) <- oddEven v
+      b        <- worker (r .^ 2) ev
+      c        <- worker (r .^ 2) od
+      let k = n `div` 2
+      m <- newSTRef (r .^ 0)
+      forM_ [0, 1 .. k - 1] $ \ix -> do
+        bi <- MV.unsafeRead b ix
+        ci <- MV.unsafeRead c ix
+        m' <- readSTRef m
+        MV.unsafeWrite v ix       (bi .+ (inverse m' .* ci))
+        MV.unsafeWrite v (ix + k) (bi .- (inverse m' .* ci))
+        modifySTRef' m (.* r)
+      return v
+
 
 -- actually no need to rearrange
 rearrange :: [a] -> [a]
